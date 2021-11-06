0b06e2938bad6885c0a8dc8def17ca2f66e2c9e7
Igor Nobrega <igor.nobrega1@hotmail.com>
2021-11-05 22:54:15 -0300

    Adicionando nova rota

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './auth/auth/login/login.component'
 import { RegisterComponent } from './auth/auth/register/register.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 const routes: Routes = [
-  {path: "", pathMatch: "full", redirectTo:"login"},
+  {path: "", pathMatch: "full", redirectTo:"dashboard"},
+  {path: "dashboard", component: DashboardComponent},
   {path: "login", component: LoginComponent},
   {path: "register", component: RegisterComponent}
 ];
